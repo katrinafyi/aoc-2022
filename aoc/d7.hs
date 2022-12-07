43d893212886fb523a6ad6347347e5900d5591bb
Kait Lam <k@rina.fyi>
2022-12-07 21:30:44 +1000

    Revert "simplify to essence."
    This reverts commit be17f00cac6eae8d8e2a63031043106049635f00.

--- aoc/d7.hs
+++ aoc/d7.hs
@@ -46,34 +46,42 @@ parseFile p = do
   s <- uint
   char ' '
   l <- line'
-  pure $ File s <$> tails p
+  pure $ File s (l : p)
 
 parseLs p = do 
   string "ls\n"
   items <- many $ eitherA (parseDir p) (parseFile p)
   let (dirs,files) = partitionEithers items
-  pure $ concat files 
+  pure $ Ls p dirs files 
 
 parseCommands p = (eof $> []) +++ do 
   string "$ "
   cmd <- eitherA (parseCd p) (parseLs p)
   case cmd of 
     Left p -> parseCommands p 
-    Right ls -> (ls ++) <$> parseCommands p 
+    Right ls -> (ls :) <$> parseCommands p 
 
 parse = readp $ do 
   parseCommands []
 
-makeDisk :: [File] -> Map.Map Path Int
-makeDisk = Map.fromListWith (+) . fmap (\(File s p) -> (p, s))
+makeDisk :: [Ls] -> Map.Map Path Dir
+makeDisk = foldr' (flip go) Map.empty 
+  where
+    reify disk ds = (disk !) <$> ds
 
-one = sum . filter (<= 100000) . Map.elems
+    go disk (Ls p ds fs) = 
+      Map.insert p (Dir sz ds' fs) disk
+      where 
+        ds' = reify disk ds
+        sz = sum $ fmap size ds' ++ fmap size fs
+
+one = sum . filter (<= 100000) . fmap size . Map.elems
 
 total = 70000000
 
-two disk = filter (>= need) . sort . Map.elems $ disk
+two disk = filter (>= need) . sort . fmap size . Map.elems $ disk
   where 
-    free = total - (disk ! [])
+    free = total - (size $ disk ! [])
     need = 30000000 - free
 
 
