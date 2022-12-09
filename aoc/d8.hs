4f9653b787fdd59bafd5925df3f98f900cbefd30
Kait Lam <k@rina.fyi>
2022-12-09 18:06:14 +1000

    d8.

--- aoc/d8.hs
+++ aoc/d8.hs
@@ -16,12 +16,6 @@ import Data.Foldable
 import Text.ParserCombinators.ReadP
 import qualified Text.ParserCombinators.ReadP as P
 
-
-type Path = [String] --deriving (Eq, Ord, Show)
-
-data File = File Int Path deriving (Eq, Ord, Show)
-data Dir = Dir Int [Dir] [File] deriving (Eq, Ord, Show)
-
 grid :: [[a]] -> [[((Int,Int),a)]]
 grid arg = do
   (r,row) <- zip [0..] arg
@@ -40,47 +34,35 @@ visible = go (-1)
       | snd x > h = Set.insert (fst x) $ go (snd x) xs
       | otherwise = go h xs
 
-one trees = --length $ Set.unions $ concat [right,left,down,up]
-  (right)
+one trees = length $ Set.unions $ concat [right,left,down,up]
   where
     right = map visible trees
     left = map (visible . reverse) trees
     down = map visible (transpose trees)
     up = map (visible . reverse) (transpose trees)
 
-two inp' = score <$> trees
+two inp = score <$> trees
   where
     size = maximum $ (\((x,_),_) -> x) <$> concat inp
 
-    inp = fmap (fmap snd) inp'
+    heights = fmap (fmap snd) inp
+    trees = concat inp
 
-    right = inp
-    left = fmap reverse inp
-    down = transpose inp
-    up = fmap reverse (transpose inp)
+    right = heights
+    left = fmap reverse heights
+    down = transpose heights
+    up = fmap reverse (transpose heights)
 
-    make = id
+    sight (h:trees) = see ++ take 1 rest
+      where (see,rest) = span (< h) trees
 
     -- score :: (Int,Int) -> Int
-    score (pos@(r,c),h) = (pos,(a,b,x,y))
+    score (pos@(r,c),_) = product $ length <$> [a,b,x,y]
       where 
-        s = size
-        sight h trees = length $ see ++ take 1 rest
-          where (see,rest) = span (< h) trees
-        a = abs $ (-) c $ maybe s snd $ Set.lookupGT pos (right !! r)
-        b = abs $ (-) c $ maybe 0 snd $ Set.lookupLT pos (left !! r)
-        x = abs $ (-) r $ maybe s fst $ Set.lookupGT pos (down !! c)
-        y = abs $ (-) r $ maybe 0 fst $ Set.lookupLT pos (up !! c)
-
-
-
-    -- score = 
-
-
-
-    -- make :: [((Int,Int),Int)] -> Set (Int,Int)
-    -- make = Set.fromList . fmap fst
-    -- make = id
+        a = sight $ drop c (right !! r)
+        b = sight $ drop (size-c) (left !! r)
+        x = sight $ drop r (down !! c)
+        y = sight $ drop (size-r) (up !! c)
 
 main :: IO ()
 main = do
@@ -89,4 +71,4 @@ main = do
   print $ inp
   print $ one inp
   -- print $ one inp
-  print $ two inp
+  print $ maximum $ two inp
